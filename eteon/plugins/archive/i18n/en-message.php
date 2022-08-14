619a9ea3975fbbab6163a0d83a0d5376beaf1099
Aleksander Haugas <aleksander.haugas@outlook.com>
2022-08-14 01:09:29 +0000

    Create en-message.php

---
+++ eteon/plugins/archive/i18n/en-message.php
@@ -0,0 +1,34 @@
+<?php
+	/*
+		Version: 		0.9.4
+		WebSite: 		http://eteon.airzox.com
+		Licensed:		AirZox Technologies
+		License-key:	KJXS-NMAL-004D-V15A
+		Developed by: 	Aleksander Haugas (Eteon MVC)
+		
+		Copyright (C) 2021, AirZox All rights reserved.
+		
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+		THE SOFTWARE.
+	*/
+
+return array(
+    'An error occurred trying to save the settings.' => 'An error occurred trying to save the settings.',
+    'Archive' => 'Archive',
+    'Could not save settings, no settings found.' => 'Could not save settings, no settings found.',
+    'Do you want to generate dates for the URLs?' => 'Do you want to generate dates for the URLs?',
+    'Generate dates' => 'Generate dates',
+    'No' => 'No',
+    'Provides an Archive pagetype behaving similar to a blog or news archive.' => 'Provides an Archive pagetype behaving similar to a blog or news archive.',
+    'Save' => 'Save',
+    'Settings' => 'Settings',
+    'The settings have been saved.' => 'The settings have been saved.',
+    'Unable to delete plugin settings.' => 'Unable to delete plugin settings.',
+    'Yes' => 'Yes',
+    'You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.' => 'You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.'
+);
