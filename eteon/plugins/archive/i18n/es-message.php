7eb9185b1172f09d24ba15af850c775ac788aa30
Aleksander Haugas <aleksander.haugas@outlook.com>
2022-08-14 01:09:54 +0000

    Create es-message.php

---
+++ eteon/plugins/archive/i18n/es-message.php
@@ -0,0 +1,34 @@
+<?php
+	/*
+		Version: 		0.9.4
+		WebSite: 		http://eteon.airzox.com
+		Licensed:		AirZox Technologies
+		License-key:	KJXS-NMAL-004D-V15A
+		Developed by: 	Aleksander Haugas (Eteon MVC)
+		
+		Copyright (C) 2021, AirZox All rights reserved.
+		
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+		THE SOFTWARE.
+	*/
+
+return array(
+    'An error occurred trying to save the settings.' => 'Se ha producido un error al intentar guardar la configuración.',
+    'Archive' => 'Archivo',
+    'Could not save settings, no settings found.' => 'No se pueden guardar los ajustes, no se encontró la configuración.',
+    'Do you want to generate dates for the URLs?' => '¿Desea generar fechas de la URL?',
+    'Generate dates' => 'Generar fechas',
+    'No' => 'No',
+    'Provides an Archive pagetype behaving similar to a blog or news archive.' => 'Proporciona un tipo de página similar al de un blog o archivo de noticias.',
+    'Save' => 'Registrar',
+    'Settings' => 'Parámetros',
+    'The settings have been saved.' => 'Se han registrado los ajustes.',
+    'Unable to delete plugin settings.' => 'No se puede eliminar la configuración de la extensión.',
+    'Yes' => 'Sí',
+    'You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.' => 'Ha modificado esta página. Si deja esta página sin guardar los datos, los cambios se perderán.'
+);
