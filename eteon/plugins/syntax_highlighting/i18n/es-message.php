88de788ca1f13ecf97d70a9ab75e24666e2f726f
Aleksander Haugas <aleksander.haugas@outlook.com>
2022-08-22 11:18:39 +0000

    Create es-message.php

---
+++ eteon/plugins/syntax_highlighting/i18n/es-message.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+		Version: 		0.9.4
+		WebSite: 		http://eteon.airzox.com
+		Licensed:		AirZox Technologies
+		License-key:	KJXS-NMAL-004D-V15A
+		Developed by: 	Aleksander Haugas (Eteon MVC)
+		
+		Copyright (C) 2021, AirZox All rights reserved.
+		
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+		THE SOFTWARE.
+	*/
+
+    return array(
+    	'Syntax highlighting' => 'Syntax highlighting',
+    	'Accepts the code to highlight (string) and a list of options (object).' => 'Acepta el código para resaltar (cadena) y una lista de opciones (objeto).',
+	    'Successfully deactivated syntax_highlighting plugin!' => '¡Se desactivó con éxito el complemento syntax_highlighting!',
+	    'Supports "Ctrl-S" keystroke to save form and continue editing.' => 'Admite la pulsación de tecla "Ctrl-S" para guardar el formulario y continuar editando.',
+    	'Successfully activated syntax_highlighting plugin!' => '¡Complemento syntax_highlighting activado con éxito!'
+    );
