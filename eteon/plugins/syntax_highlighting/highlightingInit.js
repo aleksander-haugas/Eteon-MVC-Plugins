caab3c474a5a0c4da3d499a4a93458c181409da5
Aleksander Haugas <aleksander.haugas@outlook.com>
2022-08-22 11:16:45 +0000

    Add files via upload

---
+++ eteon/plugins/syntax_highlighting/highlightingInit.js
@@ -0,0 +1,40 @@
+/*
+    Version: 		0.9.4
+    WebSite: 		http://eteon.airzox.com
+    Licensed:		AirZox Technologies
+    License-key:	KJXS-NMAL-004D-V15A
+    Developed by: 	Aleksander Haugas (Eteon MVC)
+    
+    Copyright (C) 2021, AirZox All rights reserved.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+document.addEventListener('DOMContentLoaded', (event) => {
+    let highlight_elements = document.querySelector('.hljs');
+    if (highlight_elements !== null) {
+
+        // Load syntax highlighting css styles
+        var cssId = 'myCss';  // you could encode the css path itself to generate id..
+        if (!document.getElementById(cssId)) {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = '/eteon/plugins/syntax_highlighting/styles/base16/zenburn.min.css';
+        link.media = 'all';
+        head.appendChild(link);
+        }
+
+        // Load syntax highlighting js
+        document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+        });
+    }
+});
